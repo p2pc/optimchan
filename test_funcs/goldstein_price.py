e839846d0cedba5d6652f2ca226afcb2df9f7fce
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-19 01:17:19 +0700

    add test funct for optimizer testing

--- test_funcs/goldstein_price.py
+++ test_funcs/goldstein_price.py
@@ -7,10 +7,10 @@ from autograd import grad
 class GoldsteinPrice:
     def __init__(self):
         super(GoldsteinPrice, self).__init__()
-        self.xmin, self.xmax = -1.5, 5
-        self.ymin, self.ymax = -3, 1.5
-        self.y_start, self.x_start = -2.8, 2.4  # Start point
-        self.x_optimum, self.y_optimum, self.z_optimum = 3, 0.5, 0  # Global optimum
+        self.xmin, self.xmax = -2, 2
+        self.ymin, self.ymax = -2, 2
+        self.y_start, self.x_start = -1.8, 0.4  # Start point
+        self.x_optimum, self.y_optimum, self.z_optimum = 0, -1, 3  # Global optimum
         self._compute_derivatives()
 
     def _compute_derivatives(self):
@@ -20,6 +20,5 @@ class GoldsteinPrice:
         self.df_dy = grad(self.eval, 1)
 
     def eval(self, x, y):
-        z = np.log(1+(1.5-x+x*y)**2 + (2.25-x+x*y**2)
-                   ** 2 + (2.625-x+x*y**3)**2)/10
+        z = np.log((1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14 * y + 6 * x * y + 3*y**2)) * (30 + (2*x - 3*y)**2 * (18 - 32*x + 12 * x ** 2 + 48 * y - 36 *x * y + 27 * y ** 2)))
         return z
