3e385ae8930e5c44bca812ecc55295da20106aad
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-19 09:42:45 +0700

    update nesterov momentum description

--- optimizers/gradient_descent_nesterov_momentum.py
+++ optimizers/gradient_descent_nesterov_momentum.py
@@ -1,5 +1,16 @@
 from .base import Optimizer
 
+"""
+Issue of momentum: do not slow down enough at the bottom of a valley and tend to overshoot the valley floor. Nesterov momentum: modifies to use the gradient at the projected future position.
+
+\begin{equation}
+	\textbf{v}^{(k+1)} = \beta\textbf{v}^{(k)} - \alpha\nabla f(\textbf{x}^{(k)} + \beta\textbf{v}^{(k)})
+\end{equation}
+
+\begin{equation}
+	\textbf{x}^{(k+1)} = \textbf{x}^{(k)} + \textbf{v}^{(k+1)}
+\end{equation}
+"""
 
 class GradientDescentNesterovMomentum(Optimizer):
     def __init__(self, cost_f, lr=0.001, beta=0.9, x=None, y=None):
@@ -28,7 +39,7 @@ class GradientDescentNesterovMomentum(Optimizer):
 
             self.vx = beta * self.vx + lr * dx_in_vx
             self.vy = beta * self.vy + lr * dy_in_vy
-            
+
         self.x += - self.vx
         self.y += - self.vy
 
