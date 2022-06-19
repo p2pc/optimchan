50aa973af7638d2d2dd2e8686357ed46dea28d22
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-19 22:47:24 +0700

    add

---
+++ test_funcs/sphere.py
@@ -0,0 +1,22 @@
+import autograd.numpy as np
+from autograd import grad
+import math
+
+class Sphere:
+    def __init__(self):
+        super(Sphere, self).__init__()
+        self.xmin, self.xmax = -5, 5
+        self.ymin, self.ymax = -5, 5
+        self.y_start, self.x_start = -2.8, 2.4  # Start point
+        self.x_optimum, self.y_optimum, self.z_optimum = 0, 0, 0  # Global optimum
+        self._compute_derivatives()
+
+    def _compute_derivatives(self):
+        # Partial derivative of the objective function over x
+        self.df_dx = grad(self.eval, 0)
+        # Partial derivative of the objective function over y
+        self.df_dy = grad(self.eval, 1)
+
+    def eval(self, x, y):
+        z = x**2 + y**2
+        return z
