4eaec81d4833803cfef7e772a957627915bc8de6
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-19 09:42:17 +0700

    update adam description

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -1,6 +1,35 @@
 from .base import Optimizer
 import numpy as np
 
+"""
+The adaptive moment estimation method or Adam combine advantages of RMSProp and Adadelta, momentum. The equations applied during each iteration:
+\begin{enumerate}
+    \item biased decaying momentum
+		\begin{equation}
+			\textbf{v}^{(k+1)} = \gamma_v\textbf{v}^{(k)} + (1-\gamma_v)\textbf{g}^{(k)}
+		\end{equation}
+
+    \item biased decaying sq. gradient
+	    \begin{equation}
+		    \textbf{s}^{(k+1)} = \gamma_s\textbf{s}^{(k)} + (1-\gamma_s)\left(\textbf{g}^{(k)} \odot \textbf{g}^{(k)}\right)
+		\end{equation}
+
+    \item corrected decaying momentum
+		\begin{equation}
+			\hat{\textbf{v}}^{(k+1)} = \textbf{v}^{(k+1)} / (1-\gamma_v^k)
+		\end{equation}
+
+    \item corrected decaying sq. gradient
+		\begin{equation}
+			\hat{\textbf{s}}^{(k+1)} = \textbf{s}^{(k+1)} / (1-\gamma_s^k)
+		\end{equation}
+
+    \item next iterate
+		\begin{equation}
+			\textbf{x}^{(k+1)} = \textbf{x}^{(k)} - \alpha\hat{\textbf{v}}^{(k+1)} / \left(\epsilon + \sqrt{\hat{\textbf{s}}^{(k+1)}}\right)
+		\end{equation}
+\end{enumerate}
+"""
 
 class Adam(Optimizer):
     def __init__(self, cost_f, lr=0.001, beta_1=0.9, beta_2=0.999, x=None, y=None):
