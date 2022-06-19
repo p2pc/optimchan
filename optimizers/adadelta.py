c1faf294d44ac0499202431c36b8f317680a40aa
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-19 09:41:59 +0700

    update adadelta description

--- optimizers/adadelta.py
+++ optimizers/adadelta.py
@@ -1,6 +1,13 @@
 from .base import Optimizer
 import numpy as np
 
+"""
+Adadelta is another method for overcoming Adagrad’s monotonically decreasing learning rate
+
+\begin{equation}
+    x_i^{(k+1)} = x_i^{(k)} - \frac{RMS(\Delta x_i)}{\epsilon + RMS(g_i)}g_i^{(k)}
+\end{equation}
+"""
 
 class AdaDelta(Optimizer):
     def __init__(self, cost_f, lr=0.001, decay_rate=0.9, x=None, y=None):
@@ -18,11 +25,11 @@ class AdaDelta(Optimizer):
         if not decay_rate:
             decay_rate = self.decay_rate
 
-        # derivative
+        # Derivative
         f = self.cost_f.eval(self.x, self.y)
         dx = self.cost_f.df_dx(self.x, self.y)
         dy = self.cost_f.df_dy(self.x, self.y)
-        
+
         # Update decays
         self.decay_x = decay_rate * (self.decay_x) + (1-decay_rate)*dx**2
         self.decay_y = decay_rate * (self.decay_y) + (1-decay_rate)*dy**2
