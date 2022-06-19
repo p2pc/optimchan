ca64974f1f7b4c7694cf21cc68d1ceb7b52a2627
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-19 22:46:43 +0700

    add

--- test_funcs/__init__.py
+++ test_funcs/__init__.py
@@ -5,9 +5,15 @@ from __future__ import print_function
 from .beale import Beale
 from .goldstein_price import GoldsteinPrice
 from .rosenbrock import Rosenbrock
+from .rastrigin import Rastrigin
+from .ackley import Ackley
+from .sphere import Sphere
 
 __all__ = [
     'Beale',
     'GoldsteinPrice',
-    'Rosenbrock'
+    'Rosenbrock',
+    'Rastrigin',
+    'Ackley',
+    'Sphere',
 ]
