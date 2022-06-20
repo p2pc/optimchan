d3c89d4b486fcb4483684b7afc3aa3e75e8dcfce
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-20 13:21:49 +0700

    update

--- test_funcs/goldstein_price.py
+++ test_funcs/goldstein_price.py
@@ -20,5 +20,6 @@ class GoldsteinPrice:
         self.df_dy = grad(self.eval, 1)
 
     def eval(self, x, y):
-        z = np.log((1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14 * y + 6 * x * y + 3*y**2)) * (30 + (2*x - 3*y)**2 * (18 - 32*x + 12 * x ** 2 + 48 * y - 36 *x * y + 27 * y ** 2))) / 10
+        z = np.log((1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14 * y + 6 * x * y + 3*y**2)) *
+                   (30 + (2*x - 3*y)**2 * (18 - 32*x + 12 * x ** 2 + 48 * y - 36 * x * y + 27 * y ** 2))) / 10
         return z
