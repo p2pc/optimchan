07d0bb4de38bc20b5a850ef009287d89c12a9264
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-20 13:22:04 +0700

    update

---
+++ test_funcs/mccormick.py
@@ -0,0 +1,24 @@
+import autograd.numpy as np
+from autograd import grad
+import math
+
+
+class McCormick:
+    def __init__(self):
+        super(McCormick, self).__init__()
+        self.xmin, self.xmax = -1.5, 4
+        self.ymin, self.ymax = -3, 4
+        self.y_start, self.x_start = -1.8, 0.4  # Start point
+        self.x_optimum, self.y_optimum, self.z_optimum = - \
+            0.54719, -1.54719, -1.9133  # Global optimum
+        self._compute_derivatives()
+
+    def _compute_derivatives(self):
+        # Partial derivative of the objective function over x
+        self.df_dx = grad(self.eval, 0)
+        # Partial derivative of the objective function over y
+        self.df_dy = grad(self.eval, 1)
+
+    def eval(self, x, y):
+        z = math.sin(x+y) + (x-y)**2 - 1.5*x + 2.5*y + 1
+        return z
