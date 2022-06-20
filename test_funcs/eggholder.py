07ab1ad5932141bf0907bad08b8dcaaf394dbe0a
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-20 13:21:46 +0700

    update

---
+++ test_funcs/eggholder.py
@@ -0,0 +1,25 @@
+import autograd.numpy as np
+from autograd import grad
+import math
+
+
+class EggHolder:
+    def __init__(self):
+        super(EggHolder, self).__init__()
+        self.xmin, self.xmax = -512, -512
+        self.ymin, self.ymax = -512, 512
+        self.y_start, self.x_start = -2.8, 2.4  # Start point
+        self.x_optimum, self.y_optimum, self.z_optimum = 512, 404.2319, - \
+            959.6407  # Global optimum
+        self._compute_derivatives()
+
+    def _compute_derivatives(self):
+        # Partial derivative of the objective function over x
+        self.df_dx = grad(self.eval, 0)
+        # Partial derivative of the objective function over y
+        self.df_dy = grad(self.eval, 1)
+
+    def eval(self, x, y):
+        z = -(y + 47)*math.sin(math.sqrt(abs(x/2 + (y + 47)))) - \
+            x * math.sin(math.sqrt(abs(x - (y+47))))
+        return z
