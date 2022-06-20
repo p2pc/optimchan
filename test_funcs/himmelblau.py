279c1ca37b12fcd247aa5ca96a0de57880c7d54b
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-20 13:21:52 +0700

    update

--- test_funcs/himmelblau.py
+++ test_funcs/himmelblau.py
@@ -2,6 +2,7 @@ import autograd.numpy as np
 from autograd import grad
 import math
 
+
 class Himmelblau:
     def __init__(self):
         super(Himmelblau, self).__init__()
@@ -9,9 +10,12 @@ class Himmelblau:
         self.ymin, self.ymax = -5, 5
         self.y_start, self.x_start = -2.8, 2.4  # Start point
         self.x_optimum, self.y_optimum, self.z_optimum = 3, 2, 0  # Global optimum 1
-        self.x_optimum_1, self.y_optimum_1, self.z_optimum_1 = -2.805118, 3.131312, 0  # Global optimum 2
-        self.x_optimum_2, self.y_optimum_2, self.z_optimum_2 = -3.779310, -3.283186, 0  # Global optimum 3
-        self.x_optimum_3, self.y_optimum_3, self.z_optimum_3 = -3.584428, -1.848126, 0  # Global optimum 4
+        self.x_optimum_1, self.y_optimum_1, self.z_optimum_1 = - \
+            2.805118, 3.131312, 0  # Global optimum 2
+        self.x_optimum_2, self.y_optimum_2, self.z_optimum_2 = - \
+            3.779310, -3.283186, 0  # Global optimum 3
+        self.x_optimum_3, self.y_optimum_3, self.z_optimum_3 = - \
+            3.584428, -1.848126, 0  # Global optimum 4
         self._compute_derivatives()
 
     def _compute_derivatives(self):
