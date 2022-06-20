bd940eebfa788b7d1d7d5bfd97c775b554644ed5
nhutnamhcmus <sakura9x016@gmail.com>
2022-06-20 13:21:20 +0700

    update

--- test_funcs/__init__.py
+++ test_funcs/__init__.py
@@ -2,18 +2,44 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .beale import Beale
-from .goldstein_price import GoldsteinPrice
-from .rosenbrock import Rosenbrock
-from .rastrigin import Rastrigin
 from .ackley import Ackley
+from .beale import Beale
+from .booth import Booth
+from .bukin import Bukin
+from .cross_in_tray import CrossInTray
+from .easom import Easom
+from .eggholder import EggHolder
+from .goldstein_price import GoldsteinPrice
+from .himmelblau import Himmelblau
+from .holder_table import HolderTable
+from .levi import Levi
+from .matyas import Matyas
+from .mccormick import McCormick
+from .rastrigin import Rastrigin
+from .rosenbrock import Rosenbrock
+from .schaffer_func_n2 import SchafferN2
+from .schaffer_func_n4 import SchafferN4
 from .sphere import Sphere
+from .three_hump_camel import ThreeHumpCamel
 
 __all__ = [
-    'Beale',
-    'GoldsteinPrice',
-    'Rosenbrock',
-    'Rastrigin',
     'Ackley',
+    'Beale',
+    'Booth',
+    'Bukin',
+    'CrossInTray',
+    'Easom',
+    'EggHolder',
+    'GoldsteinPrice',
+    'Himmelblau',
+    'HolderTable',
+    'Levi',
+    'Matyas',
+    'McCormick',
+    'Rastrigin',
+    'Rosenbrock',
+    'SchafferN2',
+    'SchafferN4',
     'Sphere',
+    'ThreeHumpCamel'
 ]
